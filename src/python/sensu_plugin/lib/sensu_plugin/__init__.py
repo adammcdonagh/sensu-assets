49d62809f97b377a295bc6d0780f7965d933e191
Adam McDonagh <27215476+adammcdonagh@users.noreply.github.com>
2023-05-25 10:49:36 +0000

    Fix tests

--- src/python/sensu_plugin/lib/sensu_plugin/__init__.py
+++ src/python/sensu_plugin/lib/sensu_plugin/__init__.py
@@ -3,6 +3,6 @@
 from sensu_plugin.asset import EnvDefault, SensuAsset
 from sensu_plugin.logging import init_logging, write_log_file
 from sensu_plugin.threshold import Threshold
-from sensu_plugin.check import SensuPluginCheck
 from sensu_plugin.exithook import ExitHook
 from sensu_plugin.plugin import SensuPlugin
+from sensu_plugin.check import SensuPluginCheck
